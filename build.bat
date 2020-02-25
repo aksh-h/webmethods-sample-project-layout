76d6cb47ba8ca2e937b70ccae14f0385d5043e38
Akshay H <38463158+aksh-h@users.noreply.github.com>
2020-02-25 15:02:52 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-%SAG_HOME%/common/lib/ant/bin/ant -DSAGHome=%SAG_HOME% -DSAG_CI_HOME=%SAG_CI_HOME% -DprojectName=${env.JOB_NAME} build
+%SAG_HOME%/common/lib/ant/bin/ant -DSAGHome=%SAG_HOME% -DSAG_CI_HOME=%SAG_CI_HOME% -DprojectName=$(Build.BuildName)build
